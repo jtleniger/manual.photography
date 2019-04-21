fd8292a03354479cd3fdef0b9559789b6845a40c
Justin Leniger <justin.leniger@gmail.com>
2019-04-21 12:28:12 -0600

    finished content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-    <section class="hero is-info">
+    <section class="hero is-success">
       <div class="hero-body">
         <h1 class="title">
           Manual Photography
@@ -38,13 +38,13 @@
         Aperture
       </h1>
       <p>
-        Aperture is a fancy word for an adjustable hole inside the lens that can open and close. A low number means wide open, like this:
+        Aperture is a fancy word for an adjustable hole inside the lens that can open and close. A low number means wide open:
       </p>
       <div class="aperture-container">
         <div class="aperture wide"></div>
       </div>
       <p>
-        A high number means narrow, like this:
+        A high number means narrow:
       </p>
       <div class="aperture-container">
         <div class="aperture narrow"></div>
@@ -60,18 +60,14 @@
       <p>
         The shutter is like a opening and closing a door in front of the sensor. The shutter speed is how fast the camera does it.
       </p>
-      <section class="section">
-        <p>A small number (bottom of the fraction is big) is fast, like this:
-        <div class="shutter fast">
-          <div class="curtain top"></div>
-        </div>
-      </section>
-      <section class="section">
-        <p>A big number (bottom of the fraction is small or the number is more than one) is slow, like this:
-        <div class="shutter slow">
-          <div class="curtain"></div>
-        </div>
-      </section>
+      <p>A small number (bottom of the fraction is big) is fast:
+      <div class="shutter fast">
+        <div class="curtain top"></div>
+      </div>
+      <p>A big number (bottom of the fraction is small or the number is more than one) is slow:
+      <div class="shutter slow">
+        <div class="curtain"></div>
+      </div>
       <p>
         If the shutter is open for longer, more light gets in. If your picture is too dark or too bright, changing this can fix it.
       </p>
@@ -81,13 +77,81 @@
         ISO
       </h1>
       <p>
-
+        ISO is basically a digital volume knob for the camera. It's kind of like turning up the brightness on your phone. There's not much else to say.
+      </p>
+    </section>
+    <section class="section">
+      <h1 class="title">
+        But wait! Why can't this be automatic?
+      </h1>
+      <p>
+        Each of these settings affect the picture, and have consequences, so you gotta juggle them.
+      </p>
+    </section>
+    <section class="section">
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-primary">
+            <p class="title">Aperture</p>
+            <p class="subtitle">Make it fancy</p>
+            <div class="content">
+              <p>
+                In addition to light, aperture affects your "depth-of-field", which is a fancy way to say how much stuff is in focus.
+              </p>
+              <p>
+                A big aperture (small f number) means the depth-of-field is <em>shorter</em>. Stuff behind and in front of your focus point will be blurry.
+                A small aperture makes the depth-of-field longer, so only stuff really far away from your focus point is blurry.
+              </p>
+              <p>
+                In a portrait, you might want this, in a landscape, you may not.
+              </p>
+            </div>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-info">
+            <p class="title">Shutter Speed</p>
+            <p class="subtitle">Make it blurry</p>
+            <div class="content">
+              <p>
+                If something in the picture is moving, or you aren't using a tripod, too long of a shutter speed means a blurry picture. Simple enough.
+              </p>
+              <p>
+                This can be used for effect if you want smooth water or clouds.
+              </p>
+            </div>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-warning">
+            <p class="title">ISO</p>
+            <p class="subtitle">Make it noisy</p>
+            <div class="content">
+              <p>
+                ISO adds noise. That's pretty much the only affect. The higher it gets, dark areas look more like this:
+              </p>
+              <img src="~/assets/noisy-berries.jpg">
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <h1 class="title">
+        That's it.
+      </h1>
+      <p>
+        That's pretty much all you need to know about manual mode, skipping hours of convoluted YouTube and ad-ridden articles.
       </p>
     </section>
   </section>
 </template>
 
 <style>
+
+img {
+  margin: 8px 0;
+}
 
 .overexposed img {
   filter: brightness(300%);
