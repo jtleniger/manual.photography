f8669af37d3b9aedf12d8b99bec7c046cf23a40c
Justin Leniger <justin.leniger@gmail.com>
2019-04-22 11:50:47 -0600

    navbar

---
+++ layouts/default.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item brand" href="https://tldr.photography">
+                    TL;DR
+                </a>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                    @click="showNav = !showNav" :class="{ 'is-active': showNav }"
+                    data-target="navbar">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbar" class="navbar-menu" :class="{ 'is-active': showNav }">
+                <div class="navbar-start">
+                    <nuxt-link to="/" class="navbar-item">
+                        Basics
+                    </nuxt-link>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                        More
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <nuxt-link to="/aperture" class="navbar-item">
+                                Aperture
+                            </nuxt-link>
+                            <nuxt-link to="/iso" class="navbar-item">
+                                ISO
+                            </nuxt-link>
+                            <nuxt-link to="/shutter-speed" class="navbar-item">
+                                Shutter Speed
+                            </nuxt-link>
+                            <nuxt-link to="/focal-length" class="navbar-item">
+                                Focal Length
+                            </nuxt-link>
+
+                            <hr class="navbar-divider">
+
+                            <nuxt-link to="/contact" class="navbar-item">
+                                Contact
+                            </nuxt-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <nuxt />
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>&copy; {{ new Date().getFullYear() }}, Justin Leniger</p>
+                <p>If you found this helpful and are feeling generous, help me pay for the site:</p>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_donations" />
+                <input type="hidden" name="business" value="WBYXJZMKLXWEJ" />
+                <input type="hidden" name="item_name" value="Hosting / Domain Costs" />
+                <input type="hidden" name="currency_code" value="USD" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit"
+                    title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showNav: false
+        }
+    },
+    watch: {
+        '$route' () {
+            this.showNav = false;
+        }
+  }
+}
+</script>
+
+
+<style>
+a.brand {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,232 +1,221 @@
 <template>
-  <section class="container">
-    <section class="hero is-success">
+  <div>
+    <section class="hero is-light">
       <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Manual Mode
+          </h1>
+          <h2 class="subtitle">
+            TL;DR
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <section class="section">
         <h1 class="title">
-          Manual Photography
-        </h1>
-        <h2 class="subtitle">
-          TL;DR
-        </h2>
-      </div>
-    </section>
-    <section class="section">
-      <h1 class="title">
-        Basics
-      </h1>
-      <p>
-        A picture is basically capturing light.
-      </p>
-      <p>
-        If you get too much, your picture might look like this:
-      </p>
-      <div class="overexposed">
-        <img src="~/assets/berries.jpg" alt="Photo of Berries">
-      </div>
-      <p>
-        If you get too little, your picture might look like this:
-      </p>
-      <div class="underexposed">
-        <img src="~/assets/berries.jpg" alt="Photo of Berries">
-      </div>
-      <p>
-        We don't want that. We're trying to go full goldilocks, so tweak stuff to get it right.
-      </p>
-    </section>
-    <section class="section">
-      <h1 class="title">What to Tweak</h1>
-      <section class="section tight">
-        <h1 class="title is-size-4">
-          Aperture
+          Basics
         </h1>
         <p>
-          Aperture is a fancy word for an adjustable hole inside the lens.
+          A picture is basically capturing light.
         </p>
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <div class="tile is-child example">
-              <p class="has-text-centered">
-                A low number (like f1.8) means wide open:
-              </p>
-              <div class="aperture-container">
-                <div class="aperture wide"></div>
-              </div>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <div class="tile is-child example">
-              <p class="has-text-centered">
-                A high number (like f22) means narrow:
-              </p>
-              <div class="aperture-container">
-                <div class="aperture narrow"></div>
-              </div>
-            </div>
-          </div>
+        <p>
+          If you get too much, your picture might look like this:
+        </p>
+        <div class="overexposed">
+          <img src="~/assets/berries.jpg" alt="Photo of Berries">
         </div>
         <p>
-          A big hole lets through more light than the small one. If your picture is too dark or too bright, changing this can fix it.
+          If you get too little, your picture might look like this:
+        </p>
+        <div class="underexposed">
+          <img src="~/assets/berries.jpg" alt="Photo of Berries">
+        </div>
+        <p>
+          We don't want that. We're trying to go full goldilocks, so tweak stuff to get it right.
         </p>
       </section>
-      <section class="section tight">
-        <h1 class="title is-size-4">
-          Shutter Speed
-        </h1>
-        <p>
-          The shutter basically passes an opening in front of the sensor. The shutter speed is how fast the camera does it.
-        </p>
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <div class="tile is-child example">
-              <p class="has-text-centered">A small number (like 1/1000s) is fast:
-              <div class="shutter fast">
-                <div class="curtain">
-                  <div class="top"></div>
-                  <div class="middle"></div>
-                  <div class="bottom"></div>
+      <section class="section">
+        <h1 class="title">What to Tweak</h1>
+        <section class="section tight">
+          <h1 class="title is-size-4">
+            Aperture
+          </h1>
+          <p>
+            Aperture is a fancy word for an adjustable hole inside the lens.
+          </p>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <div class="tile is-child example">
+                <p class="has-text-centered">
+                  A low number (like f1.8) means wide open:
+                </p>
+                <div class="aperture-container">
+                  <div class="aperture wide"></div>
+                </div>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child example">
+                <p class="has-text-centered">
+                  A high number (like f22) means narrow:
+                </p>
+                <div class="aperture-container">
+                  <div class="aperture narrow"></div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="tile is-parent">
-            <div class="tile is-child example">
-              <p class="has-text-centered">A big number (like 1s) is slow:
-              <div class="shutter slow">
-                <div class="curtain">
-                  <div class="top"></div>
-                  <div class="middle"></div>
-                  <div class="bottom"></div>
+          <p>
+            A big hole lets through more light than the small one. If your picture is too dark or too bright, changing this can fix it.
+          </p>
+        </section>
+        <section class="section tight">
+          <h1 class="title is-size-4">
+            Shutter Speed
+          </h1>
+          <p>
+            The shutter basically passes an opening in front of the sensor. The shutter speed is how fast the camera does it.
+          </p>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <div class="tile is-child example">
+                <p class="has-text-centered">A small number (like 1/1000s) is fast:
+                <div class="shutter fast">
+                  <div class="curtain">
+                    <div class="top"></div>
+                    <div class="middle"></div>
+                    <div class="bottom"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child example">
+                <p class="has-text-centered">A big number (like 1s) is slow:
+                <div class="shutter slow">
+                  <div class="curtain">
+                    <div class="top"></div>
+                    <div class="middle"></div>
+                    <div class="bottom"></div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          <p>
+            If the shutter is open for longer, more light gets in. If your picture is too dark or too bright, changing this can fix it.
+          </p>
+        </section>
+        <section class="section tight">
+          <h1 class="title is-size-4">
+            ISO
+          </h1>
+          <p>
+            ISO is basically a digital volume knob for the camera. It's kind of like turning up the brightness on your phone.
+            If your picture is too dark or too bright, changing this can fix it.
+          </p>
+        </section>
+      </section>
+      <section class="section">
+        <h1 class="title">
+          How do I know if I've gotten it right?
+        </h1>
         <p>
-          If the shutter is open for longer, more light gets in. If your picture is too dark or too bright, changing this can fix it.
+          Most mirrorless and DSLR cameras with manual mode have a thing on the screen or viewfinder that looks something like this:
+        </p>
+        <img class="meter" src="~/assets/exposure.svg" alt="Exposure Meter">
+        <p>
+          Tweak your settings to get the those little tick tacks below the scale near the 0, and then snap a photo.
+        </p>
+      </section>
+      <section class="section">
+        <h1 class="title">
+          But wait! Why would I want to set these myself?
+        </h1>
+        <p>
+          Each of these settings has its own effect the picture, so if you successfully juggle them, you can control those effects.
         </p>
       </section>
       <section class="section tight">
-        <h1 class="title is-size-4">
-          ISO
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-primary">
+              <p class="title">Aperture</p>
+              <p class="subtitle">Make It Fancy or Make It Crisp</p>
+              <div class="content">
+                <p>
+                  In addition to light, aperture affects your "depth-of-field", which is a fancy way to say how much stuff is in focus.
+                </p>
+                <p>
+                  Depth of field is like taking a giant slice of cake out of the space in front of you. Stuff in that slice is sharp.
+                </p>
+                <p>
+                  A big hole means the slice of cake is <em>shorter</em>. Stuff behind and in front of your focus point will be blurry.
+                </p>
+                <img src="~/assets/f1_8.jpg" alt="f1.8">
+                <p>
+                  A small hole makes the slice of cake very <em>long</em>, so almost everything is in focus.
+                </p>
+                <img src="~/assets/f22.jpg" alt="f22">
+                <p>
+                  In a portrait, you might want a blurry background, so you would use a big hole (large aperture, small number).
+                  In a landscape, you may not, so you would use a small hole (small aperture, big number).
+                </p>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-info">
+              <p class="title">Shutter Speed</p>
+              <p class="subtitle">Don't Make It Blurry</p>
+              <div class="content">
+                <p>
+                  If something in the picture is moving or your hands are shaky, too long of a shutter speed means a blurry picture.
+                  Simple enough.
+                </p>
+                <p>
+                  Generally, you'll want to keep this as fast (low number) as possible to minimize blur.
+                </p>
+                <p>
+                  The shutter speed can be used for effect if you've got a tripod. Leave it open for days to get smooth water or clouds.
+                </p>
+              </div>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-warning">
+              <p class="title">ISO</p>
+              <p class="subtitle">Don't Make It Noisy</p>
+              <div class="content">
+                <p>
+                  ISO adds noise. That's pretty much the only effect.
+                  This one is usually used to compensate when you can't tweak the others more.
+                  The higher the number, dark areas look more and more like this:
+                </p>
+                <img src="~/assets/noisy-berries.jpg" alt="Noisy photo of Berries">
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <h1 class="title">
+          That's it.
         </h1>
         <p>
-          ISO is basically a digital volume knob for the camera. It's kind of like turning up the brightness on your phone.
-          If your picture is too dark or too bright, changing this can fix it.
+          That's pretty much all you need to know about manual.
+          Better than hours of convoluted YouTube videos and ad-ridden articles, right?
+        </p>
+        <p>
+          The only hard part is using these settings to get the result you want.
         </p>
       </section>
     </section>
-    <section class="section">
-      <h1 class="title">
-        How do I know if I've gotten it right?
-      </h1>
-      <p>
-        Most mirrorless and DSLR cameras with manual mode have a thing on the screen or viewfinder that looks something like this:
-      </p>
-      <img class="meter" src="~/assets/exposure.svg" alt="Exposure Meter">
-      <p>
-        Tweak your settings to get the those little tick tacks below the scale near the 0, and then snap a photo.
-      </p>
-    </section>
-    <section class="section">
-      <h1 class="title">
-        But wait! Why would I want to set these myself?
-      </h1>
-      <p>
-        Each of these settings has its own effect the picture, so if you successfully juggle them, you can control those effects.
-      </p>
-    </section>
-    <section class="section tight">
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-primary">
-            <p class="title">Aperture</p>
-            <p class="subtitle">Make It Fancy or Make It Crisp</p>
-            <div class="content">
-              <p>
-                In addition to light, aperture affects your "depth-of-field", which is a fancy way to say how much stuff is in focus.
-              </p>
-              <p>
-                Depth of field is like taking a giant slice of cake out of the space in front of you. Stuff in that slice is sharp.
-              </p>
-              <p>
-                A big hole means the slice of cake is <em>shorter</em>. Stuff behind and in front of your focus point will be blurry.
-              </p>
-              <img src="~/assets/f1_8.jpg" alt="f1.8">
-              <p>
-                A small hole makes the slice of cake very <em>long</em>, so almost everything is in focus.
-              </p>
-              <img src="~/assets/f22.jpg" alt="f22">
-              <p>
-                In a portrait, you might want a blurry background, so you would use a big hole (large aperture, small number).
-                In a landscape, you may not, so you would use a small hole (small aperture, big number).
-              </p>
-            </div>
-          </article>
-        </div>
-      </div>
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-info">
-            <p class="title">Shutter Speed</p>
-            <p class="subtitle">Don't Make It Blurry</p>
-            <div class="content">
-              <p>
-                If something in the picture is moving or your hands are shaky, too long of a shutter speed means a blurry picture.
-                Simple enough.
-              </p>
-              <p>
-                Generally, you'll want to keep this as fast (low number) as possible to minimize blur.
-              </p>
-              <p>
-                The shutter speed can be used for effect if you've got a tripod. Leave it open for days to get smooth water or clouds.
-              </p>
-            </div>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-warning">
-            <p class="title">ISO</p>
-            <p class="subtitle">Don't Make It Noisy</p>
-            <div class="content">
-              <p>
-                ISO adds noise. That's pretty much the only effect.
-                This one is usually used to compensate when you can't tweak the others more.
-                The higher the number, dark areas look more and more like this:
-              </p>
-              <img src="~/assets/noisy-berries.jpg" alt="Noisy photo of Berries">
-            </div>
-          </article>
-        </div>
-      </div>
-    </section>
-    <section class="section">
-      <h1 class="title">
-        That's it.
-      </h1>
-      <p>
-        That's pretty much all you need to know about manual.
-        Better than hours of convoluted YouTube videos and ad-ridden articles, right?
-      </p>
-      <p>
-        The only hard part is using these settings to get the result you want.
-      </p>
-    </section>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>&copy; {{ new Date().getFullYear() }}, Justin Leniger</p>
-        <p>If you found this helpful and are feeling generous, help me pay for the site:</p>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_donations" />
-          <input type="hidden" name="business" value="WBYXJZMKLXWEJ" />
-          <input type="hidden" name="item_name" value="Hosting / Domain Costs" />
-          <input type="hidden" name="currency_code" value="USD" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit"
-            title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
-      </div>
-    </footer>
-  </section>
+  </div>
 </template>
 
 <style>
