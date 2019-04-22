a4a8090f6c57b86eab220d10480e8ba48762e5f8
Justin Leniger <justin.leniger@gmail.com>
2019-04-22 11:07:54 -0600

    analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,13 @@ export default {
   modules: [
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
+    '@nuxtjs/google-analytics'
   ],
+
+  googleAnalytics: {
+    id: 'UA-138783300-1',
+    dev: false
+  },
 
   /*
   ** Build configuration
